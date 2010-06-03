b2dd8898659f98a14b4e835945e6572fbb8b0225
eMancu <emiliano.mancuso@grid.com>
2010-06-03 19:15:36 -0400

    Update templates

--- generators/comment/templates/create_comments.rb
+++ generators/comment/templates/create_comments.rb
@@ -1,16 +1,17 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.string :title, :limit => 50, :default => "" 
-      t.text :comment, :default => "" 
-      t.references :commentable, :polymorphic => true
-      t.references :user
+      t.string      :title, :limit => 50, :default => ""
+      t.text        :comment
+      t.references  :commentable, :polymorphic => true
+      t.references  :author,      :polymorphic => true
       t.timestamps
     end
 
     add_index :comments, :commentable_type
     add_index :comments, :commentable_id
-    add_index :comments, :user_id
+    add_index :comments, :author_id
+    add_index :comments, :author_type
   end
 
   def self.down
